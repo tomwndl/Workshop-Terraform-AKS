d0ce9804b7b74475cee2ada02c38012ec3f4e215
Stanislas Quastana <13031988+squasta@users.noreply.github.com>
2022-03-03 12:58:25 +0100

    Update acr.tf

--- Terraform/AzureContainerRegistry/acr.tf
+++ Terraform/AzureContainerRegistry/acr.tf
@@ -1,7 +1,19 @@
-## INSERER ICI LE CODE POUR FAIRE UNE Azure Container Registry avec un Role ACR PUlL pour le cluster AKS
+## INSERER ICI LE CODE POUR FAIRE UNE Azure Container Registry
 
 # Indices : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/container_registry
-#                prendre la SKU Basic
+#                prendre la SKU Standard
+
+
+resource "azurerm_container_registry" "Terra-acr" {
+  name                = var.acrName
+  resource_group_name = var.resourceGroupName
+  location            = var.azureRegion
+  sku                 = "Standard"
+  admin_enabled       = false
+  
+}
+
+
 #           https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
 #           https://docs.microsoft.com/en-us/azure/container-registry/container-registry-roles?tabs=azure-cli 
 #                  scope                = l'Azure Container Registry
