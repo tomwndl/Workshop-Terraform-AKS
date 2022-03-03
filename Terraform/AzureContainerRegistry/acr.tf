306af91b3c866b8cbd83f98ad0a69e03b3326fe7
Stanislas Quastana <squasta@microsoft.com>
2022-03-03 21:52:55 +0100

    ajout code affection Role ACRPull pour le cluster sur ACR

--- Terraform/AzureContainerRegistry/acr.tf
+++ Terraform/AzureContainerRegistry/acr.tf
@@ -13,9 +13,9 @@ resource "azurerm_container_registry" "Terra-acr" {
   
 }
 
-
-#           https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
-#           https://docs.microsoft.com/en-us/azure/container-registry/container-registry-roles?tabs=azure-cli 
-#                  scope                = l'Azure Container Registry
-#                  role_definition_name = "ACR Reader"
-#                  principal_id         = la system managed identity du cluster AKS
+# https://www.terraform.io/language/values/outputs#declaring-an-output-value
+# cette output value sera utilisée dans un datasources terraform_remote_state dans le code du cluster AKS
+# pour l'affectation du role ACR Pull au cluster AKS sur cette registry
+output "acr-id" {
+  value = azurerm_container_registry.Terra-acr.id
+}
