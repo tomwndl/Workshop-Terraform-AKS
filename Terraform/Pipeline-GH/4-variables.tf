2f3dfb677377deb2ffe541f66ee1db18fa51204e
Stanislas Quastana <squasta@microsoft.com>
2022-03-08 14:37:56 +0100

    lab workflow github

---
+++ Terraform/Pipeline-GH/4-variables.tf
@@ -0,0 +1,14 @@
+variable "azureRegion" {
+  description = "Azure Region where to deploy resources. Caution the region must support Availability Zone"
+  # To get names of Azure Region : az account list-locations
+  # To check support of Availability Zone in the Azure Region see https://docs.microsoft.com/bs-latn-ba/azure/availability-zones/az-overview
+  type    = string
+  default = "westeurope"
+}
+
+#  Resource Group Name
+variable "resourceGroupName" {
+  type    = string
+  default = "RG-DemoGitHub"
+}
+
